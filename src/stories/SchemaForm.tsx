cae4974ad2a721ddbd4f1314c58ecb2d78d7c8db
adimis <aditya.mishra@eoncollective.com>
2024-04-29 17:23:57 +0530

    folder cleanup

--- src/stories/SchemaForm.tsx
+++
@@ -1,8 +0,0 @@
-import SchemaForm from "@/components/SchemaForm/SchemaForm";
-import { ISchemaFormProps } from "@/components/SchemaForm/interface";
-
-const SchemaFormStory = (props: ISchemaFormProps) => {
-  return <SchemaForm {...props} />;
-};
-
-export default SchemaFormStory;
