3ac38b8bb383f60b9a0f8db7fce650e51f841c08
adimis <aditya.mishra@eoncollective.com>
2024-05-03 02:47:12 +0530

    type update

--- src/components/SchemaForm/index.ts
+++ src/components/SchemaForm/index.ts
@@ -2,5 +2,6 @@ import { MultiStepSchemaForm } from "./MultiStepSchemaForm/MultiStepForm";
 import { SchemaForm } from "./SchemaForm/SchemaForm";
 import "../../index.css";
 
-export * from "./interface"
+export * from "./interface";
+export * from "./fields/Fields";
 export { SchemaForm, MultiStepSchemaForm };
